d6085211d8919d26ac23d8e9d50e7ba71e171d96
m-ghiringhelli <mghiring@gmail.com>
2022-03-22 14:57:20 -0700

    added navlink and navigation in header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Main from './views/Main/Main';
 import Presidents from './views/Presidents/Presidents';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <div className="App">
         <Switch>
           <Route exact path='/'>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <ul>
+        <li>
+          <NavLink className='link' exact to='/'>
+            HOME
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className='link' to='/presidents'>
+            RECONSTRUCTION PRESIDENTS
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
