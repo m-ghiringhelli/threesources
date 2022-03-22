a13be297ddf598b0ad5da1e2d2713b9ae06f049c
m-ghiringhelli <mghiring@gmail.com>
2022-03-22 16:55:21 -0700

    added functioning search to tudors

--- src/services/tudors.js
+++ src/services/tudors.js
@@ -1,6 +1,11 @@
 import { checkError, client } from './client.js';
 
-export async function fetchTudors() {
-  const resp = await client.from('tudormonarchs').select('*').order('id', { ascending: true });
-  return checkError(resp);
+export async function fetchTudors(search) {
+  if (search) {
+    const resp = await client.from('tudormonarchs').select('*').ilike('name', `%${search}%`).order('id', { ascending: true });
+    return checkError(resp);
+  } else {
+    const resp = await client.from('tudormonarchs').select('*').order('id', { ascending: true });
+    return checkError(resp);
+  }
 }
