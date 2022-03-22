422dae1aa1f8fa94ca094417b8571756adfa33f6
m-ghiringhelli <mghiring@gmail.com>
2022-03-22 15:08:58 -0700

    sets leaders on page load

--- src/views/Soviets/Soviets.js
+++ src/views/Soviets/Soviets.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchSoviets } from '../../services/soviets';
 
 export default function Soviets() {
-  
-  fetchSoviets();
+  const [leaders, setLeaders] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await fetchSoviets();
+        setLeaders(resp);
+      } catch (e) {
+        setErrorMessage(`couldn't load soviet premiers`);
+      }
+    };
+    fetchData();
+  }, []);
+  
   return (
-    <div>Soviets</div>
+    <div>
+      
+    </div>
   );
 }
