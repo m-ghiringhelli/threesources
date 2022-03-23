c6105d473504f786db8b2365da49c6810fca1463
m-ghiringhelli <mghiring@gmail.com>
2022-03-22 17:13:37 -0700

    added test

--- src/services/tudors.js
+++ src/services/tudors.js
@@ -2,10 +2,17 @@ import { checkError, client } from './client.js';
 
 export async function fetchTudors(search) {
   if (search) {
-    const resp = await client.from('tudormonarchs').select('*').ilike('name', `%${search}%`).order('id', { ascending: true });
+    const resp = await 
+    client.from('tudormonarchs')
+      .select('*')
+      .ilike('name', `%${search}%`)
+      .order('id', { ascending: true });
     return checkError(resp);
   } else {
-    const resp = await client.from('tudormonarchs').select('*').order('id', { ascending: true });
+    const resp = await 
+    client.from('tudormonarchs')
+      .select('*')
+      .order('id', { ascending: true });
     return checkError(resp);
   }
 }
