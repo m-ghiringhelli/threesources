49ee58dd0cd565533f2594325219334b5f2d280d
m-ghiringhelli <mghiring@gmail.com>
2022-03-22 17:01:21 -0700

    works for soviets

--- src/services/soviets.js
+++ src/services/soviets.js
@@ -1,6 +1,18 @@
 import { checkError, client } from './client.js';
 
-export async function fetchSoviets() {
-  const resp = await client.from('sovietleaders').select('*').order('id', { ascending: true });
-  return checkError(resp);
+export async function fetchSoviets(search) {
+  if (search) {
+    const resp = await 
+    client.from('sovietleaders')
+      .select('*')
+      .ilike('name', `%${search}%`)
+      .order('id', { ascending: true });
+    return checkError(resp);
+  } else {
+    const resp = await 
+    client.from('sovietleaders')
+      .select('*')
+      .order('id', { ascending: true });
+    return checkError(resp);
+  }
 }
--- src/views/Soviets/Soviets.js
+++ src/views/Soviets/Soviets.js
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { fetchSoviets } from '../../services/soviets';
 import Portrait from '../../components/Portrait/Portrait';
+import Search from '../../components/Search/Search';
 
 export default function Soviets() {
   const [leaders, setLeaders] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
+  const [searchText, setSearchText] = useState('');
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const resp = await fetchSoviets();
+        const resp = await fetchSoviets(search);
         setLeaders(resp);
       } catch (e) {
         setErrorMessage(`couldn't load soviet premiers`);
       }
     };
     fetchData();
-  }, []);
+  }, [search]);
 
   return (
     <>
       <p>{errorMessage}</p>
+      <Search {...{ setSearchText, setSearch, searchText }} />
       <div>
         {leaders.map((leader) => (
           <Portrait key={leader.id} person={leader} />
