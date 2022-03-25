3411753491f1a285d1334ae6f5c128bf4742bc2c
m-ghiringhelli <mghiring@gmail.com>
2022-03-25 12:50:30 -0700

    added styling to navbar/ background color to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
   return (
     <div className='header'>
-      <ul>
+      <div className='headerText'>
+        <h1>THREEDERS</h1>
+        <h3>three groups of leaders from world history</h3>
+      </div>
+      <ul className='navbar'>
         <li>
           <NavLink className='link' exact to='/'>
             HOME
